9f57af0831b132566936770a7384666da17429b0
Josias Besodia <djodev23@gmail.com>
2024-06-13 17:14:30 +0200

    change2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};

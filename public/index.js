c1c53860f268b0f04feebeb2680102a490111818
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-06-18 18:14:10 +0100

    update

--- public/index.js
+++ public/index.js
@@ -1,3 +1,8 @@
-function canlendarShow() {
-    // document.getElementsById('calendar').style.opacity = 100;
-}
+document.getElementById('showCalendarRadio').addEventListener('change', function() {
+    var calendarVar = document.getElementById('calendar');
+    if (this.checked) {
+        calendarVar.style.display = 'block';
+    } else {
+        calendarVar.style.display = 'none';
+    }
+});

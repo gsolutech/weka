df8094915de8acddd73b8f84170e004cff9fb4c3
Josias Besodia <djodev23@gmail.com>
2024-06-18 19:36:54 +0200

    calendar show

--- public/index.js
+++ public/index.js
@@ -1,10 +1,10 @@
-function showCalendar() {
-    document.getElementById('showCalendarRadio').addEventListener('change', function() {
-        var calendarVar = document.getElementById('calendar');
-        if (this.checked) {
-            calendarVar.style.display = 'block';
-        } else {
-            calendarVar.style.display = 'none';
-        }
-    });
-}
+
+document.getElementById('showCalendarRadio').addEventListener('change', function() {
+    let calendarVar = document.getElementById('calendar');
+    if (this.checked) {
+        calendarVar.style.display = 'block';
+    } else {
+        calendarVar.style.display = 'none';
+    }
+});
+

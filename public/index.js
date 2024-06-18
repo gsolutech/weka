0581c7ab39118846127a26fa739d4eedbbb11f69
Josias Besodia <djodev23@gmail.com>
2024-06-18 19:11:24 +0200

    js change

--- public/index.js
+++ public/index.js
@@ -1,8 +1,10 @@
-document.getElementById('showCalendarRadio').addEventListener('change', function() {
-    var calendarVar = document.getElementById('calendar');
-    if (this.checked) {
-        calendarVar.style.display = 'block';
-    } else {
-        calendarVar.style.display = 'none';
-    }
-});
+function showCalendar() {
+    document.getElementById('showCalendarRadio').addEventListener('change', function() {
+        var calendarVar = document.getElementById('calendar');
+        if (this.checked) {
+            calendarVar.style.display = 'block';
+        } else {
+            calendarVar.style.display = 'none';
+        }
+    });
+}

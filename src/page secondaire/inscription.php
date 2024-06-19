edac04ac915d5ee24961cd80d4c8e1e9974845df
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-06-19 13:52:16 +0100

    update

--- src/page secondaire/inscription.php
+++ src/page secondaire/inscription.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription</title>
-    <script src="../page secondaire/inscription.js" defer></script>
+    <link href="../main.css" rel="stylesheet">
 </head>
 
 <body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,16 @@ module.exports = {
   },
   plugins: [],
 };
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+

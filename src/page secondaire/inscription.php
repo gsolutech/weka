e26894bd7f1a9318cf30cb193f4c174db08010d6
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-06-19 13:52:56 +0100

    update

--- src/page secondaire/inscription.php
+++ src/page secondaire/inscription.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <title>Inscription</title>
     <link href="../main.css" rel="stylesheet">
 </head>

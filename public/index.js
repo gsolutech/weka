290cbe1e9d46beba48a319bd143317f833e56b7f
Josias Besodia <djodev23@gmail.com>
2024-06-19 13:17:23 +0200

    js change

--- public/index.js
+++ public/index.js
@@ -1,10 +1,4 @@
-
-document.getElementById('showCalendarRadio').addEventListener('change', function() {
-    let calendarVar = document.getElementById('calendar');
-    if (this.checked) {
-        calendarVar.style.display = 'block';
-    } else {
-        calendarVar.style.display = 'none';
-    }
-});
+function canlendarShow() {
+    // document.getElementsById('calendar').style.opacity = 100;
+}
 

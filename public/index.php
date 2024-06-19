e5c83992f9510234e0fabda9cc4ef4c19f7d9eab
Josias Besodia <djodev23@gmail.com>
2024-06-19 16:58:16 +0200

    background check

--- public/index.php
+++ public/index.php
@@ -5,37 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../dist/output.css">
     <script src="index.js" defer></script>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <title>weka</title>
 </head>
 <body>
-    <section id="header">
+    <section id="header" class="w-full text-white">
         <!-- beso -->
-        <nav>
-            <ul class="firstU">
-                <img src="../src/assets/statics/Fichier 10@4xweka.png" alt="">
+        <nav class="bg-black w-full h-20 flex flex-row text-white">
+            <ul class="flex flex-row w-2/6">
+                <img src="../src/assets/statics/Fichier 10@4xweka.png" alt="" class="w-20 h-auto object-contain ml-20">
             </ul>
-            <ul class="secondU">
-                <li href="">Accueil</li>
-                <li href="">Reservation</li>
-                <li href="">Avis</li>
-                <li href="">Contact</li>
+            <ul class="items-center justify-center flex">
+                <li href="" class="pr-7 cursor-pointer">Accueil</li>
+                <li href="" class="pr-7 cursor-pointer">Reservation</li>
+                <li href="" class="pr-7 cursor-pointer">Avis</li>
+                <li href="" class="pr-7 cursor-pointer">Contact</li>
             </ul>
-            <ul class="lastU">
-                <button id="btn_inscrire">S'inscrire</button>
+            <ul class="absolute right-20 justify-end items-end flex">
+                <button id="btn_inscrire" class="mt-7 bg-cyan-500 border-2 border-solid border-cyan-500 pr-2 pl-2 rounded-lg">S'inscrire</button>
             </ul>
         </nav>
         <div class="container" style="background-image: url('/src/assets/statics/bgheader.jpg');">
             <ul>
-                <img src="../src/assets/statics/Fichier 10@4xweka.png" alt="">
+                <img src="../src/assets/statics/Fichier 10@4xweka.png" alt="" class="w-20 h-auto object-contain ml-20">
             </ul>
             <ul>
                 <p>Réservation simplifiée pour <label>Tous Vos Besoins</label></p>
             </ul>
-            <ul class="secondUl">
+            <ul class="items-center justify-center flex">
                 <input type="search" name="inputSearch" class="search">
                 <input type="submit" value="Rechercher" name="valideSearch" class="btnSend">
             </ul>
-            <ul class="lastUl">
+            <ul class="justify-end items-end flex">
                 <input type="radio" name="filtre_Check" id="" value="dateFiltre" id="showCalendarRadio">
                 <label for="filtre_Check">Filtrez par date</label>
                 <input type="radio" name="filtre_Check" id="" checked value="dispoFiltre">

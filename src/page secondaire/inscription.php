d00ae06651a6431723e9a890598f64fa65dec312
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-06-20 11:59:47 +0100

    update

--- src/page secondaire/inscription.php
+++ src/page secondaire/inscription.php
@@ -13,7 +13,7 @@
     <div class="h-screen bg-indigo-80 flex justify-center items-center">
         <div class="lg:w-2/5 md:w-1/2 w-2/3">
             <form class="bg-white p-8 rounded-lg shadow-lg min-w-full">
-                <h1 class="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">S'Enregistres</h1>
+                <h1 class="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">Enregistrez-Vous</h1>
                 <div>
                     <label class="text-gray-800 font-semibold block my-3 text-md" for="username">Nom </label>
                     <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="username" id="username" placeholder="Nom" />

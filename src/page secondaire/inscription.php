6aaed424f59a29027b0eeda9ff529789ee54db38
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-06-20 12:12:37 +0100

    update

--- src/page secondaire/inscription.php
+++ src/page secondaire/inscription.php
@@ -28,7 +28,7 @@
                 </div>
                 <div>
                     <label class="text-gray-800 font-semibold block my-3 text-md" for="password">Password</label>
-                    <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="password" id="password" placeholder="password" />
+                    <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="password" id="password" placeholder="Mots de passe" />
                 </div>
                 <div>
                     <label class="text-gray-800 font-semibold block my-3 text-md" for="confirm">Numero de telephone</label>

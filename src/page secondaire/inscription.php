c44c19750cfef6797958e3a2bbe0f2e4474bbe6b
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-06-21 15:49:09 +0100

    update

--- src/page secondaire/inscription.php
+++ src/page secondaire/inscription.php
@@ -63,7 +63,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <label class="text-gray-800 font-semibold block my-3 text-md" for="phone">Numero de telephone</label>
                     <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="phone" id="phone" placeholder="Votre Numero de telephone" required />
                 </div>
-                <button type="submit" class="w-full mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans">Enregistres</button>
+                <button type="submit" id="submit" class="w-full mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans">Enregistres</button>
                 <button type="button" class="w-full mt-6 mb-3 bg-indigo-100 rounded-lg px-4 py-2 text-lg text-gray-800 tracking-wide font-semibold font-sans">Se Connecter</button>
             </form>
         </div>

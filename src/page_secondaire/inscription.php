ce2bdbe68cbe9a89c158a990500b384064a7da45
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-06-23 20:57:50 +0100

    update

--- src/page_secondaire/inscription.php
+++ src/page_secondaire/inscription.php
@@ -31,31 +31,31 @@ if (isset($_POST['check_inscri'])) {
 ?>
 
 <div id="inscriptionShow" class="h-auto bg-indigo-80 justify-center items-center hidden z-50 backdrop-blur fixed inset-4 mt-40">
-    <div class="lg:w-2/5 md:w-1/2 w-2/3">
-        <form method="POST" class="bg-white p-8 rounded-lg shadow-lg min-w-full">
-            <h1 class="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">Enregistrez-Vous</h1>
+    <div class="w-11/12 sm:w-2/3 md:w-1/2 lg:w-2/5 mx-auto">
+        <form method="POST" class="bg-white p-4 sm:p-6 md:p-8 rounded-lg shadow-lg w-full">
+            <h1 class="text-center text-xl sm:text-2xl mb-4 sm:mb-6 text-gray-600 font-bold font-sans">Enregistrez-Vous</h1>
             <div>
-                <label class="text-gray-800 font-semibold block my-3 text-md" for="username">Nom </label>
-                <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="username" id="username" placeholder="Nom" />
+                <label class="text-gray-800 font-semibold block my-2 sm:my-3 text-sm sm:text-md" for="username">Nom </label>
+                <input class="w-full bg-gray-100 px-2 sm:px-4 py-1 sm:py-2 rounded-lg focus:outline-none" type="text" name="username" id="username" placeholder="Nom" />
             </div>
             <div>
-                <label class="text-gray-800 font-semibold block my-3 text-md" for="username">Postnom </label>
-                <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="firstname" id="firstname" placeholder="PostNom" />
+                <label class="text-gray-800 font-semibold block my-2 sm:my-3 text-sm sm:text-md" for="firstname">Postnom </label>
+                <input class="w-full bg-gray-100 px-2 sm:px-4 py-1 sm:py-2 rounded-lg focus:outline-none" type="text" name="firstname" id="firstname" placeholder="PostNom" />
             </div>
             <div>
-                <label class="text-gray-800 font-semibold block my-3 text-md" for="email">Email</label>
-                <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="email" id="email" placeholder="@email" />
+                <label class="text-gray-800 font-semibold block my-2 sm:my-3 text-sm sm:text-md" for="email">Email</label>
+                <input class="w-full bg-gray-100 px-2 sm:px-4 py-1 sm:py-2 rounded-lg focus:outline-none" type="text" name="email" id="email" placeholder="@email" />
             </div>
             <div>
-                <label class="text-gray-800 font-semibold block my-3 text-md" for="password">Mots de passe</label>
-                <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="password" id="password" placeholder="Mots de passe" />
+                <label class="text-gray-800 font-semibold block my-2 sm:my-3 text-sm sm:text-md" for="password">Mots de passe</label>
+                <input class="w-full bg-gray-100 px-2 sm:px-4 py-1 sm:py-2 rounded-lg focus:outline-none" type="text" name="password" id="password" placeholder="Mots de passe" />
             </div>
             <div>
-                <label class="text-gray-800 font-semibold block my-3 text-md" for="confirm">Numero de telephone</label>
-                <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="phone" id="phone" placeholder="Votre Numero de telephone" />
+                <label class="text-gray-800 font-semibold block my-2 sm:my-3 text-sm sm:text-md" for="phone">Numero de telephone</label>
+                <input class="w-full bg-gray-100 px-2 sm:px-4 py-1 sm:py-2 rounded-lg focus:outline-none" type="text" name="phone" id="phone" placeholder="Votre Numero de telephone" />
             </div>
-            <button type="submit" class="w-full mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans">Enregistres</button>
-            <button type="submit" name="check_inscri" class="w-full mt-6 mb-3 bg-indigo-100 rounded-lg px-4 py-2 text-lg text-gray-800 tracking-wide font-semibold font-sans">Se Connecter</button>
+            <button type="submit" class="w-full mt-4 sm:mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans">Enregistres</button>
+            <button type="button" name="check_inscri" class="w-full mt-4 sm:mt-6 mb-3 bg-indigo-100 rounded-lg px-4 py-2 text-lg text-gray-800 tracking-wide font-semibold font-sans">Se Connecter</button>
         </form>
     </div>
 </div>

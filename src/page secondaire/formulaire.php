d8b91c5df17f42c85683784d739f375fa81526f3
Goma Solution Tech <169062230+gsolutech@users.noreply.github.com>
2024-06-25 12:37:08 +0000

    Revert "connexion"
    This reverts commit 39ef40576afaa19ea866ad0162039ed816f7dde6.

--- src/page secondaire/formulaire.php
+++ src/page secondaire/formulaire.php
@@ -1,33 +1,42 @@
-    <div class="w-420 bg-slate-100">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="form.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="weka">
         <form action="">
-            <h1 class="text-3xl text-center mt-20 ">contactez nous</h1>
-            <div class="m-30 relative">
-                <input class="w-3/4 scroll-p-15 rounded-3xl text-center " type="text" placeholder="se connecter avec facebook">
+            <h1>contactez nous</h1>
+            <div class="solutech">
+                <input type="text" placeholder="se connecter avec facebook">
             </div>
-            <!-- <div class="ad">
+            <div class="ad">
                 <img src="facebook.png" alt="fb">
-            </div> -->
-            
-            <div class="m-30 relative">
-                <input class="w-3/4 scroll-p-15 rounded-3xl text-center " type="text" placeholder="se connecter avec Google">
             </div>
-             <!-- <div class="add">
+            
+            <div class="solutech">
+                <input type="text" placeholder="se connecter avec Google">
+            </div>
+             <div class="add">
                 <img src="google.png" alt="">
-             </div> -->
-           <div class="flex my-20 items-center justify-center">
-            <hr class="h-2 w-2/5 ">
-            <p class="p-30 text-3xl">ou</p>
+             </div>
+           <div class="h">
+            <hr>
+            <p>ou</p>
             <hr>
            </div>
 
-            <div class="m-30">
-                <input class="w-3/4 p-15 border-0" type="mail" placeholder="Adresse mail">
+            <div class="goma">
+                <input type="mail" placeholder="Adresse mail">
             </div>
-            <div class="m-30">
-                <input class="w-3/4 p-15 border-0" type="password" placeholder="Mots passe">
+            <div class="goma">
+                <input type="password" placeholder="Mots passe">
             </div>
             <div class="btn" align="center">
-                <input class="bg-cyan-600 px-10 py-20 border-0 decoration-white text-base rounded-md my-80" type="submit" value="Se connecter">
+                <input type="submit" value="Se connecter">
             </div>
         </form>
     </div>

75316817e0b70d124a1eb35e7a7b42c7255e2042
Josias Besodia <djodev23@gmail.com>
2024-06-25 15:57:07 +0200

    form

--- src/page_secondaire/formulaire.php
+++ src/page_secondaire/formulaire.php
@@ -1,12 +1,12 @@
 <div id="inscriptionShow" class="h-auto bg-indigo-80 justify-center items-center hidden z-50 backdrop-blur fixed inset-1 mt-40">
-    <div class="w-11/12 sm:w-2/3 md:w-1/2 lg:w-2/5 mx-auto">    
-        <form action="" class="class="bg-white p-4 sm:p-6 md:p-8 rounded-lg shadow-lg w-full">
+    <div class="w-11/12 sm:w-2/3 md:w-1/2 lg:w-2/5 mx-auto">
+        <form action="" class="class=" bg-white p-4 sm:p-6 md:p-8 rounded-lg shadow-lg w-full">
             <h1 class="text-3xl text-center mt-20 ">contactez nous</h1>
             <div class="relative">
                 <button class="w-3/4 scroll-p-15 rounded-3xl text-center " type="submit">
                     se connecter avec facebook
                     <img src="facebook.png" alt="fb">
-                <button>
+                    <button>
             </div>
             <!-- <div class="ad">
             </div> -->
@@ -16,7 +16,7 @@
             </div>
             <!-- <div class="add">
                 <img src="google.png" alt="">
-             </div> -->
+            </div> -->
             <div class="flex my-20 items-center justify-center">
                 <hr class="h-2 w-2/5 ">
                 <p class="p-30 text-3xl">ou</p>

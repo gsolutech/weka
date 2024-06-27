e7ce2e5c8809120d38c89619351d681e4e2584fc
Greg <gregmalos@gmail.com>
2024-06-27 16:17:18 +0200

    modif-1

--- public/index.php
+++ public/index.php
@@ -17,6 +17,24 @@
 
     <section>
         <!-- greg -->
+         <div class="logo">
+            
+            <div class="logo-app">
+                <img src="src/assets/statics/Fichier 10@4xweka.png" alt="weka-logo">
+            </div>
+            
+            <div class="main-logo">
+                <nav classe="main-logo-nav">
+                    <ul>
+                        <li><a href="#"> Acceuil </a></li>
+                        <li><a href="#">Reservation</a></li>
+                        <li><a href="#">Avis</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+            
+         </div>
     </section>
 </body>
 </html>

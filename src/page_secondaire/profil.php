0707a82325540d9c8eac21f7dada34f72021782c
Josias Besodia <djodev23@gmail.com>
2024-07-15 13:31:12 +0200

    profils config

---
+++ src/page_secondaire/profil.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'head.php'; ?>
+<body>
+    <?php  
+        require_once dirname(dirname(__DIR__))  . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'conBd.php';
+    ?>
+    <section>
+        <div class="photoUpload w-full h-96 relative">
+            <div class="couverture w-full h-44 bg-cyan-500 relative">
+
+            </div>
+            <div class="profilUp bg-lime-600">
+
+            </div>
+        </div>
+    </section>
+</body>
+</html>

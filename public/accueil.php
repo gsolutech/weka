b7ca629ec452b2d8f812e36909bc862cd9f2f7ca
Josias Besodia <djodev23@gmail.com>
2024-07-22 23:05:14 +0200

    change17

--- public/accueil.php
+++ public/accueil.php
@@ -32,11 +32,11 @@
                 <div class="w-full h-6 bg-yellow-400">
                     <p class="">Fermez</p>
                 </div>
-                <button class="border-b-2 border-solid border-gray-100 h-16 shadow-lg flex flex-row items-center justify-center my-auto">
-                    <div class="w-16 h-16">
+                <button class="border-b-2 border-solid border-gray-100 h-16 shadow-lg flex flex-row items-center justify-center my-auto py-6 px-4">
+                    <div class="w-16 h-16 absolute left-4 rounded-full">
                         <img src="../src/assets/salles/profil/1.jpg" alt="profil" class="w-full">
                     </div>
-                    <div>
+                    <div class="w-5/6 flex items-center justify-center pb-6">
                         <p class="text-black">Mon profil</p>
                     </div>
                 </button>

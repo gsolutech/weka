32903f4d593b61158f098d8712fce7980a548c41
Josias Besodia <djodev23@gmail.com>
2024-07-23 12:23:35 +0200

    CHANGE2

--- public/profil.php
+++ public/profil.php
@@ -19,7 +19,7 @@
                 <button class="right-10 bottom-5 absolute"><i class="fa-solid fa-pen-to-square" class="text-white"></i></button>
             </div>
             <div class="profilUp absolute w-44 h-44 rounded-full ml-20 top-20 border-8 border-solid border-white bge-cyan-custom items-center justify-center flex">
-                <p class="text-white text-6xl font-bold"><?php echo $premiereLettre ?></p>
+                <p class="text-white text-8xl font-bold"><?php echo $premiereLettre ?></p>
                 <img src="../src/assets/salles/profil/<?php echo $photo_profil_name?>" alt="" class="relative w-full h-full object-cover rounded-full hidden" id="profileImage">
                 <form action="" method="post" enctype="multipart/form-data">
                     <input type="file" name="profile_picture" id="showFilesDialog" class="hidden" accept="image/*">

de8acd957e7c5102a20970efc85cee7e6f881740
Josias Besodia <djodev23@gmail.com>
2024-07-25 15:38:14 +0200

    change16

--- public/profil.php
+++ public/profil.php
@@ -17,9 +17,7 @@
     ?>
     <section>
         <div id="main_container" class="photoUpload w-full h-96 relative flex flex-col text-center">
-            <ul class="w-24 h-24 bg-red-500 rounded-full absolute left-4 top-4">
-                <i class='bx bx-left-arrow-alt'></i>
-            </ul>
+
             <label for="showFilesDialogCouv">
                 <div class="w-full h-44 bge-cyan-custom relative overflow-hidden" id="couverture_image_container">
                     <img src="../src/assets/salles/couverture/<?php echo $photo_couverture_name?>" alt="" class="relative w-full h-full object-cover object-center hidden" id="couvertureImage">          
@@ -45,6 +43,9 @@
                     </form>
                 </div>
             </label>
+            <ul class="w-12 h-12 bg-red-500 rounded-full absolute left-6 top-6 items-center justify-center flex">
+                <i class='bx bx-left-arrow-alt'></i>
+            </ul>
             <p class="text-red-600 font-bold"><?php echo $error ?></p>
         </div>
     </section>
